b62981db9b492e3ab83eab73baf6a5d786357bd8
Saurabh <souravbhkumar@gmail.com>
2023-08-15 16:48:37 +0530

    Initial commit

---
+++ javascript/array.js
@@ -0,0 +1,8 @@
+var marks=[10,34,54,33,22,11];
+console.log(marks);
+console.log("___")
+var marks2=new Array();
+marks[0]=67;
+marks[1]=90;
+marks[2]=54;
+console.log(marks2);
---
+++ javascript/script.js
@@ -0,0 +1,8 @@
+var a=1
+var b=2
+console.log(a+b)
+console.log("external js")
+for(let i=1;i<10;i++)
+{
+    console.log("hello"+i);
+}
